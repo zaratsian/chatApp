d57373aea09609af55bf8bf77bca3c52ad373da3
Dan Zaratsian <dtz001@gmail.com>
2021-09-03 11:57:46 -0400

    misc updates

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'xyzantidote123'
+app.config['SECRET_KEY'] = 'xyzchatapp123'
 socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    #socketio.emit('my response', json, callback=messageReceived)
-    emit('my response', json)
+@socketio.on('event')
+def handle_my_custom_event(json_payload, methods=['GET', 'POST']):
+    #print(f'Received event: {json_payload}')
+    emit('response', json_payload)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
 
 
